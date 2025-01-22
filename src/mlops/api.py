3c956b50ea17d45a6460a1dfc9ef324a130b6358
Evangelos Lamprou <vagos@lamprou.xyz>
2025-01-21 22:39:12 -0500

    Fix api port
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- src/mlops/api.py
+++ src/mlops/api.py
@@ -83,4 +83,5 @@ async def generate_text(request: TextGenerationRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
